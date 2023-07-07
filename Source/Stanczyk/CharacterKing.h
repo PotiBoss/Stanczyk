0dbb3c18512a98d2fadd152cd4c84c22df5520a5
Mikołaj Potera <potigimboss@gmail.com>
2023-07-07 21:43:18 +0200

    Added base classes and folder structure

---
+++ Source/Stanczyk/CharacterKing.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+#include "CharacterKing.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANCZYK_API ACharacterKing : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Stanczyk/CharacterStanczyk.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+#include "CharacterStanczyk.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANCZYK_API ACharacterStanczyk : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
