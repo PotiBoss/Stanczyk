99ef5ad618fcd64e71b8c35c996857bb63fb1576
Mikołaj Potera <potigimboss@gmail.com>
2023-07-07 21:22:09 +0200

    Initial commit

---
+++ Source/Stanczyk/Stanczyk.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Stanczyk.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Stanczyk, "Stanczyk" );
+
+DEFINE_LOG_CATEGORY(LogStanczyk)
+ 
